1eade79c7f001283eb5955fdb6d9a1a4f24d6374
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-11-28 14:38:27 +0900

    ws connected

--- README.md
+++ README.md
@@ -25,3 +25,31 @@ Zoom Clone using Node.js, WebRTC and WebSockets
    server.js는 BackEnd에서 구동되고 app.js는 FrontEnd에서 구동된다.
 
    babel-node를 실행시키면 babel-node는 바로 babel.config.json을 찾고 코드에 적용돼야 하는 preset을 실행시킨다.
+
+3. WS사용
+
+   > npm i ws
+
+   ReferenceError: \_\_dirname is not defined in ES module scope
+
+이 오류는 ES 모듈로 간주되어 \_\_dirname이 정의되지 않았기 때문에 발생한다. 오류 메시지에 따르면 파일이 .js 확장자를 가지고 있고 package.json 파일에 "type": "module"이 설정되어 있어 ES 모듈로 처리되고 있다고 나와 있다.
+
+ES 모듈에서는 \_\_dirname 변수를 사용할 수 없다. 대신에 import.meta.url을 사용하여 현재 모듈의 경로를 얻을 수 있다.
+
+따라서, \_\_dirname을 사용하는 부분을 import.meta.url로 변경해야 한다. 예를 들면 다음과 같이 변경할 수 있다.
+
+```js
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+```
+
+위의 코드를 사용하면 ES 모듈에서도 \_\_dirname을 사용할 수 있게 된다. import.meta.url을 사용하여 현재 모듈의 URL을 얻고, fileURLToPath 함수를 사용하여 URL을 파일 경로로 변환한 다음, dirname 함수를 사용하여 디렉토리 경로를 얻는다.
+
+이렇게 변경하여 \_\_dirname이 정의되어 오류가 해결되었다.
+
+4. WebSocket()
+
+   > `var aWebSocket = new WebSocket(url, [, protocols]);`
--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 // express로 할 일: views를 설정해주고 render 해주기
 // 나머지는 websocket에서 실시간으로 일어날예정
+
+// ES 모듈로 간주되어 __dirname이 정의되지 않았기 때문에 발생
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// ES 모듈에서는 __dirname 변수를 사용할 수 없습니다.
+// 대신에 import.meta.url을 사용하여 현재 모듈의 경로를 얻을 수 있습니다.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -14,4 +25,16 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 // catchall url을 만들고 싶다면 위의 코드처럼 작성, 유저가 어떤 url로 이동하던지 홈으로 이동하도록 설정
 const handleListen = () => console.log(`Listen on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// 여기서 서버만들기 createServer를 하려면 requestListener 경로가 있어야된다.
+const server = http.createServer(app);
+// 같은 서버에서 http, webSocket 둘 다 작동시키기
+const wss = new WebSocketServer({ server });
+
+function handleConnection() {
+  console.log("ok");
+}
+
+wss.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
